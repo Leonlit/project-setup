60ebe526862f05e4b4b386cdc8d75d41115c6fbc
Leon <leonlit123@gmail.com>
2020-09-12 15:12:37 +0800

    Added readme file contents, updated version and more error handling in the cli file

---
+++ Readme.md
@@ -0,0 +1,24 @@
+# project-setup
+A simple tool for generating project folders and files according to programmer's use case
+
+## Setting up
+
+To get started, please clone this repo to any folder in your machine. Then, run
+```
+npm install -g <the path to the location that you cloned the project to>
+```
+
+Now, you can start using the command to generate folders and files for specific projects. To see what options are available, just run ```project-setup``` in your terminal and a list of project types will be shown. After that, in the desired location, run ```project-setup <type of projects>``` in your terminal and the files and folder will be automatically generated.
+
+
+## Types of project
+
+
+                Name                  |         Command          
+     -------------------------------- | ------------------------ 
+        Basic web application         |      basic-web-app       
+      Basic heroku application        |     heroku-basic-php     
+        Basic Php application         |      php-basic-app       
+      Php and MySQL application       |      php-mysql-app       
+     NodeJS basic console application |     node-console-app    
+     NodeJS basic Express application |     node-express-app     
--- cli.js
+++ cli.js
@@ -8,36 +8,52 @@ const {herokuBasicPhp} = require("./project-types/heroku-basic-php");
 const location = process.cwd();
 const projectType = process.argv[2];
 
-console.log(location);
 
-const typesList = ["node-console-app", "node-express-app", "basic-web-app", "php-basic-app", "php-mysql-app", "heroku-basic-php"];
+const typesList = ["node-console-app",
+                    "node-express-app",
+                    "basic-web-app",
+                    "php-basic-app",
+                    "php-mysql-app",
+                    "heroku-basic-php"
+                ];
 
-switch (projectType) {
-    case typesList[0]:
-        nodeConsoleApp(location);
-        break;
-    case typesList[1]:
-        nodeExpressApp(location);
-        break;
-    case typesList[2]:
-        basicWebApp(location);
-        break;
-    case typesList[3]:
-        basicWebApp(location);
-        break;
-    case typesList[4]:
-        phpMysqlApp(location);
-        break;
-    case typesList[5]:
-        herokuBasicPhp(location);
-        break;
-    default:
-        console.log("To generate files and folders for specific type of project, please provide one of the types of the projects provided below:\n");
-        let list = "";
-        typesList.forEach(type => {
-            list += `${type} | `;
-        });
-        console.log(list, "\n");
+if (projectType === undefined) {
+    console.log("Error!!! Expected user to provide the program with one arguments to specify the types of project to generate files and folder for.\n");
+    displayProjectTypes();
+}else {
+    console.log(location);
+    switch (projectType) {
+        case typesList[0]:
+            nodeConsoleApp(location);
+            break;
+        case typesList[1]:
+            nodeExpressApp(location);
+            break;
+        case typesList[2]:
+            basicWebApp(location);
+            break;
+        case typesList[3]:
+            basicWebApp(location);
+            break;
+        case typesList[4]:
+            phpMysqlApp(location);
+            break;
+        case typesList[5]:
+            herokuBasicPhp(location);
+            break;
+        default:
+            console.log("Invalid arguments!!! Please make sure the project types are correct. \n");
+            displayProjectTypes();
+    }
+}
+
+function displayProjectTypes () {
+    console.log("To generate files and folders for specific type of project, please provide one of the types of the projects provided below:\n");
+    let list = "";
+    typesList.forEach(type => {
+        list += `${type} | `;
+    });
+    console.log(list, "\n");
 }
 //check which command choosed
 //call files according to type
